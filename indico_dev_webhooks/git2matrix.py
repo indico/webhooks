c51629b1ee826969ce1ea19b6c00b267064baa73
Adrian Moennich <adrian.moennich@cern.ch>
2018-08-27 15:00:14 +0200

    Initial import

---
+++ indico_dev_webhooks/git2matrix.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+
+import bleach
+import markupsafe
+import requests
+from flask import Flask, jsonify, request
+
+
+try:
+    MATRIX_TOKEN = os.environ['MATRIX_TOKEN']
+    MATRIX_CHANNEL = os.environ['MATRIX_CHANNEL']
+except KeyError as exc:
+    print(f'Required environment variable not set: {exc}')
+    sys.exit(1)
+
+
+def matrix_post_msg(message):
+    url = f'https://matrix.org/_matrix/client/r0/rooms/{MATRIX_CHANNEL}/send/m.room.message/f{int(time.time())}'
+    payload = {
+        'msgtype': 'm.text',
+        'body': bleach.clean(message, strip=True),
+        'format': 'org.matrix.custom.html',
+        'formatted_body': message,
+    }
+    requests.put(url, params={'access_token': MATRIX_TOKEN}, json=payload).raise_for_status()
+
+
+app = Flask(__name__)
+
+
+@app.route('/github')
+def webhook_github():
+    extra = markupsafe.escape(request.json['msg'])
+    matrix_post_msg(f'This is just a <font color="red">test</font> - {extra}')
+    return jsonify()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from setuptools import find_packages, setup
+
+setup(
+    name='indico-dev-webhooks',
+    version='1.0',
+    url='https://github.com/indico/webhooks',
+    license='MIT',
+    author='Adrian Moennich',
+    author_email='adrian.moennich@cern.ch',
+    description='Indico dev webhooks',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['bleach', 'flask', 'requests', 'gunicorn'],
+)
